a1b6b077798c6c692a489e9183d8279937ddecb3
Ramkarthik Krishnamurthy <ramkarthikkrishnamurthy@gmail.com>
2020-06-28 23:40:26 -0600

    Compress GIFs and disable default load for all currencies

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,16 @@
 class Footer extends React.Component{
     render(){
         return (
-            <footer>
-                <div className="content has-text-centered mt-2">
-                    <p>Built by <a href="https://kramkarthik.com">Ramkarthik</a> | <a href="https://twitter.com/Ramkarthik/">Twitter</a> | <a href="https://github.com/Ramkarthik/ForeignExchange">GitHub</a> | <a href="https://netlify.com/">Hosted on Netlify</a> | Data Source: <a href="https://exchangeratesapi.io/">Foreign exchange rates API</a></p>
+            <footer class="footer">
+                <div class="content has-text-centered">
+                <p>Built by <a href="https://kramkarthik.com">Ramkarthik</a> | <a href="https://twitter.com/Ramkarthik/">Twitter</a> | <a href="https://github.com/Ramkarthik/ForeignExchange">GitHub</a> | <a href="https://netlify.com/">Hosted on Netlify</a> | Data Source: <a href="https://exchangeratesapi.io/">Foreign exchange rates API</a></p>
                 </div>
             </footer>
+            // <footer>
+            //     <div className="content has-text-centered mt-2">
+            //         <p>Built by <a href="https://kramkarthik.com">Ramkarthik</a> | <a href="https://twitter.com/Ramkarthik/">Twitter</a> | <a href="https://github.com/Ramkarthik/ForeignExchange">GitHub</a> | <a href="https://netlify.com/">Hosted on Netlify</a> | Data Source: <a href="https://exchangeratesapi.io/">Foreign exchange rates API</a></p>
+            //     </div>
+            // </footer>
         )
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -14,19 +14,6 @@ class Index extends React.Component {
         this.currencyFormCallback = this.currencyFormCallback.bind(this)
     }
 
-    componentDidMount(){
-        const today = date.today()
-        const previousDay = date.previousDay()
-        this.setState({loading: false})
-        const api = "https://api.exchangeratesapi.io/history?start_at=" + previousDay + "&end_at=" + today + "&base=USD"
-        fetch(api).then(res => {
-           return res.json()
-            
-        }).then(data => {
-            this.setState({currencies: data.rates, loading: false, base: "USD" })
-        })
-    }
-
     currencyFormCallback = (data) => {
         const today = date.today()
         const previousDay = date.previousDay()
@@ -48,9 +35,6 @@ class Index extends React.Component {
                     <CurrencyForm currencyFormCallback={this.currencyFormCallback} />
                     <div style={{display: this.state.loading ? 'none' : 'block'}}>
                         <Currencies base={this.state.base} currencies={this.state.currencies} />
-                    </div>
-                    <div style={{display: this.state.loading ? 'block' : 'none'}}>
-                        <progress className="progress is-small is-primary" max="100">15%</progress>
                     </div>
                 </Layout>
             </div>
